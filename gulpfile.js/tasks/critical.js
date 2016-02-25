ce559a2d38e74c593abd970cba114c203bbbd7f4
Connor Freeman <connormfreeman@gmail.com>
2016-02-25 02:15:47 -0600

    removed debugging styles

--- gulpfile.js/tasks/critical.js
+++ gulpfile.js/tasks/critical.js
@@ -1,21 +1,21 @@
-// var gulp        = require('gulp')
-// var critical    = require('critical')
-// var path        = require('path')
-// var config      = require('../config')
-// var manifest    = require('../../public/rev-manifest.json')
+var gulp        = require('gulp')
+var critical    = require('critical')
+var path        = require('path')
+var config      = require('../config')
+var manifest    = require('../../public/rev-manifest.json')
 
-// var criticalTask = function (cb) {
-//     critical.generate({
-//         inline: true,
-//         minify: true,
-//         base: config.root.dest,
-//         src: 'index.html',
-//         dest: path.join(config.root.dest, 'index.html'),
-//         css: [path.join(config.root.dest, manifest['stylesheets/global.css'])],
-//         width: 1300,
-//         height: 900
-//     });
-// }
+var criticalTask = function (cb) {
+    critical.generate({
+        inline: true,
+        minify: true,
+        base: config.root.dest,
+        src: 'index.html',
+        dest: path.join(config.root.dest, 'index.html'),
+        css: [path.join(config.root.dest, manifest['stylesheets/global.css'])],
+        width: 1300,
+        height: 900
+    });
+}
 
-// gulp.task('critical', criticalTask)
-// module.exports = criticalTask
+gulp.task('critical', criticalTask)
+module.exports = criticalTask
