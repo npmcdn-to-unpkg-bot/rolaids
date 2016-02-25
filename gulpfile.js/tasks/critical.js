216753b49f9ca67c5ff312fe29490ebd20bc0ae8
Connor Freeman <connormfreeman@gmail.com>
2016-02-25 02:49:14 -0600

    added critical css to all pages

--- gulpfile.js/tasks/critical.js
+++ gulpfile.js/tasks/critical.js
@@ -1,20 +1,20 @@
 var gulp        = require('gulp')
-var critical    = require('critical')
+var critical    = require('critical').stream
 var path        = require('path')
 var config      = require('../config')
 var manifest    = require('../../public/rev-manifest.json')
 
 var criticalTask = function (cb) {
-    critical.generate({
-        inline: true,
-        minify: true,
-        base: config.root.dest,
-        src: 'index.html',
-        dest: path.join(config.root.dest, 'index.html'),
-        css: [path.join(config.root.dest, manifest['stylesheets/global.css'])],
-        width: 1300,
-        height: 900
-    });
+  return gulp.src(path.join(config.root.dest, '*.html'))
+    .pipe(critical({
+          inline: true,
+          minify: true,
+          base: config.root.dest,
+          css: [path.join(config.root.dest, manifest['stylesheets/global.css'])],
+          width: 1300,
+          height: 900
+    }))
+    .pipe(gulp.dest(config.root.dest));
 }
 
 gulp.task('critical', criticalTask)
